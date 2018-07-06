c101bf616e721d7b2de0e527f84fb557ec6a3905
Yi-Yun Cheng <yiyunyc2@illinois.edu>
2018-07-06 14:13:50 -0500

    Update plotdata.py

--- plotdata.py
+++ plotdata.py
@@ -1,11 +1,17 @@
-#to get this module to work, you have to read into a Darwin-core based species occurence dataset and also do these first two commands:
-#import pandas as pd
+#to get this module to work, you have to:
+#1. install the libraries: numpy, pandas, matplotlib, seaborn, Basemap
+#installation instruction of seaborn library: https://seaborn.pydata.org/installing.html
+#installation instruction of Basemap: https://matplotlib.org/basemap/users/installing.html
+#2. read into a Darwin-core based species occurence dataset, such as: 
 #d1=pd.read_csv("FILEPATH.csv",error_bad_lines=False)
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mpl_toolkits.basemap import Basemap
+
 def plotdata(d,t): #d= datasets; t= taxonomy name in text. e.g. plotdata (d1, "Weakley 2014")
-	import matplotlib.pyplot as plt
-	import seaborn as sns
-	from mpl_toolkits.basemap import Basemap
 	sns.lmplot( x="decimalLongitude", y="decimalLatitude", data=d, fit_reg=False, hue='scientificName', legend=False, size=10, aspect=1.5)
 	plt.legend(loc='lower right')
 	plt.title(t)
